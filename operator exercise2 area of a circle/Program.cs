74b6ef7f58d3d2c51c79a96f29947ce0bf7da6c2
Aaron <git@github.com:aromero082/PizzaAlgo.git>
2021-11-01 17:15:46 -0700

    initial commit

---
+++ operator exercise2 area of a circle/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace operator_exercise2_area_of_a_circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  //exercise 2
+            string var = "pi * r ^ 2";
+            Console.WriteLine("What is the radious of your circle?");
+            Console.WriteLine(20);
+            var radious = double.Parse(Console.ReadLine());
+            var radius = 20 * radious ; string v = ("pi * 20 ^ 2");
+            double v1 = Math.PI * 20 * radius;
+            double.Parse("pi * 20 ^ 2");
+            //Outpus("The area of a circle with radius of 20 is 1256.6370614359173");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    } 
+}
